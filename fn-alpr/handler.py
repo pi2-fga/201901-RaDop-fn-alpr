ccd3953ef37c23972e55e31125b302c48dd48b91
João Pedro Sconetto <sconetto.joao@gmail.com>
2019-04-27 17:17:19 -0300

    Fn-ALPR alpha version
    Evolve the function to connect via API to the Open ALPR
    
    Add omit file in the gitignore
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- fn-alpr/handler.py
+++ fn-alpr/handler.py
@@ -1,16 +1,18 @@
 import base64
 import json
 import requests
+import uuid
 
 URL = 'https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=0' \
       '&country=br&secret_key={}'
 FN_CALL = 'alpr-call'
+KEY_LENGTH = 27
 
 def decode(encoded_key: str):
     try:
         decoded = base64.b64decode(encoded_key, validate=True)
         decoded = decoded.decode('utf-8').rstrip('\n')
-        if len(decoded) != 27:
+        if len(decoded) != KEY_LENGTH:
             raise Exception('The sent key isn\'t specified as expected.\n' \
                             'Verify the sent key!!')
     except base64.binascii.Error as e:
@@ -30,15 +32,27 @@ def decode(encoded_key: str):
 def openalpr_api(private_key: str, image: str):
     try:
         url = URL.format(private_key)
-        image = bytes(image)
+        image = bytes(image, 'utf-8')
         request = requests.post(url, data=image)
+        response = request.json()
+        if 'error_code' in response.keys():
+            code = response['error_code']
+            msg = response['error']
+            raise Exception(f'Error while calling ALPR API.\nTraceback:' \
+                            f' {code} - {msg}')
+        elif 'data_type' in response.keys() and 'alpr_results' in response['data_type']:
+            identifier = str(uuid.uuid4())
+            response['uuid'] = identifier
+        else:
+            raise Exception(f'Unknown condition while receiving data from' \
+                            f'ALPR API. Object received {response}.')
     except Exception as e:
         status_code = 500
-        msg = e
+        msg = e.args[0]
         response = {'status_code': status_code, 'response_message': msg}
         return response
     else:
-        return {} # [WIP] function
+        return response
 
 
 def handle(req):
@@ -48,14 +62,12 @@ def handle(req):
     """
     try:
         package = json.loads(req)
-
         request_type = package['type']
         request_payload = package['payload']
-        request_time = package['time']
         key = request_payload['key']
         image = request_payload['image']
 
-        if request_type == FN_CALL:
+        if request_type != FN_CALL:
             status_code = 500
             msg = 'Error!! The message header indicates other function call.' \
                   'Verify if the correct service was called.'
@@ -64,7 +76,7 @@ def handle(req):
         if not key:
             status_code = 500
             msg = 'Error trying to get the ALPR API key!!' \
-                'Verify service to correct error'
+                  'Verify service to correct error'
             response = {'status_code': status_code, 'response_message': msg}
             return json.dumps(response)
         
@@ -74,6 +86,7 @@ def handle(req):
             error = decoded_key
             return json.dumps(error)
         
+        call = openalpr_api(decoded_key, image)
         
     except Exception as e:
         status_code = 500
@@ -82,4 +95,4 @@ def handle(req):
         response = {'status_code': status_code, 'response_message': msg}
         return json.dumps(response)
     else:
-        return json.dumps({'key': decoded_key})
+        return json.dumps({'status_code': 200, 'response': call})
